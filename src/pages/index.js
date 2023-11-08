b970ec64613a5d4a45ecc31557576934032d46a8
Samarth Chauhan <samarth.chauhan13022002@gmail.com>
2023-11-08 16:13:48 +0530

    login-error

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,11 +19,31 @@ const Home = ({ user, cart, addtoCart, removefromCart }) => {
       }
     }
   `;
-  const { loading, error, data } = useQuery(GET_ALL_ITEMS);
+
+  const [loadingData, setLoadingData] = useState(true);
+  const { error, data } = useQuery(GET_ALL_ITEMS);
   const [items, setItems] = useState([]);
+
+  const handleButton = () => {
+    if (!user.email) {
+      return toast.error("Please login first to order items", {
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } else {
+      addtoCart(item.id, item.itemName, 1, item.itemPrice);
+    }
+  };
   useEffect(() => {
     if (data) {
       setItems(data.allItems);
+      setLoadingData(false);
     }
   }, [data]);
   if (error) {
@@ -58,40 +78,43 @@ const Home = ({ user, cart, addtoCart, removefromCart }) => {
         pauseOnHover
         theme="dark"
       />
-      <div className=" bg-slate-900 text-white flex flex-auto px-5 py-24 mx-auto">
-        {items.map((item) => (
-          <div
-            key={item.id}
-            className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5"
-          >
-            <div className="flex mt-4 text-center">
-              <div className="mr-4">
-                <Image
-                  className="m-auto block"
-                  src={item.itemImage}
-                  alt={item.itemName}
-                  width={100}
-                  height={80}
-                ></Image>
-              </div>
-              <div className="text-left">
-                <h3 className="text-white text-lg mb-1">{item.itemName}</h3>
-                <p className="text-white text-sm "> {item.itemDesc}</p>
-                <p className="mt-1 text-sm">₹ {item.itemPrice}</p>
-                <button
-                  disabled={!user.email}
-                  onClick={() =>
-                    addtoCart(item.id, item.itemName, 1, item.itemPrice)
-                  }
-                  className="flex m-2 text-sm text-white bg-slate-700 
-                  disabled:bg-slate-800 border-white border-2 py-1  md:px-2 focus:outline-none hover:bg-slate-600 rounded"
-                >
-                  Add it!
-                </button>
+      <div className="min-h-screen bg-slate-900 text-white flex flex-auto px-5 py-24 mx-auto">
+        {loadingData ? (
+          <div className="fixed top-0 left-0 w-screen h-screen z-[99999999999999] flex items-center justify-center bg-black/40">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+          </div>
+        ) : (
+          items.map((item) => (
+            <div
+              key={item.id}
+              className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5"
+            >
+              <div className="flex mt-4 text-center">
+                <div className="mr-4">
+                  <Image
+                    className="m-auto block"
+                    src={item.itemImage}
+                    alt={item.itemName}
+                    width={100}
+                    height={80}
+                  ></Image>
+                </div>
+                <div className="text-left">
+                  <h3 className="text-white text-lg mb-1">{item.itemName}</h3>
+                  <p className="text-white text-sm "> {item.itemDesc}</p>
+                  <p className="mt-1 text-sm">₹ {item.itemPrice}</p>
+                  <button
+                    onClick={handleButton}
+                    className="flex m-2 text-sm text-white bg-slate-700 
+                  border-white border-2 py-1  md:px-2 focus:outline-none hover:bg-slate-600 rounded "
+                  >
+                    Add it!
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </>
   );
